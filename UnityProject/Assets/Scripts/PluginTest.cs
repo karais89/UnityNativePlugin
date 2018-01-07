adcd5d7395cd5fb3159561af27943558fadb2a6f
JH <karais89@gmail.com>
2018-01-07 21:24:04 +0900

    유니티 안드로이드 통신 스크립트 추가 유니티 안드로이드 jar 파일 추가 유니티 안드로이드 매니페스트 추가 유니티 패키지명 변경

---
+++ UnityProject/Assets/Scripts/PluginTest.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+public class PluginTest : MonoBehaviour {
+ 
+    string messge = "init";
+    AndroidJavaClass myCls;
+    AndroidJavaObject myObj;
+ 
+    //// Use this for initialization
+    void Start () {
+        myCls = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        myObj = myCls.GetStatic<AndroidJavaObject>("currentActivity");       
+        Debug.Log("myCls : " + myCls != null ? myCls.ToString() : "");
+        Debug.Log("MyObj : " + myObj != null ? myObj.ToString() : "");
+    }
+    
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(100, 100, 100, 50), "StaticInt"))
+        {  
+            messge += ("\n" + myObj.CallStatic<int>("GetStaticInt", 123).ToString());
+        }
+ 
+        if (GUI.Button(new Rect(100, 200, 100, 50), "StaticString"))
+        {
+            messge += ("\n" + myObj.CallStatic<string>("GetStaticString", "StaticString"));
+        }
+ 
+        if (GUI.Button(new Rect(100, 300, 100, 50), "Int"))
+        {
+            messge += ("\n" + myObj.Call<int>("GetInt", 456).ToString());
+        }
+ 
+        if (GUI.Button(new Rect(100, 400, 100, 50), "String"))
+        {
+            messge += ("\n" + myObj.Call<string>("GetString", "String"));
+        }
+       
+        GUI.Label(new Rect(Screen.width / 2 - 350, Screen.height / 2 - 150, 700, 300), messge);
+    }
+}
