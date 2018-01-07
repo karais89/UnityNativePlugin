1bcefd0a9abba65ec40148841ef709413105ff43
JH <karais89@gmail.com>
2018-01-07 23:42:36 +0900

    유니티 안드로이드 통신 예제 완성 하지만 정상 작동 안함

---
+++ UnityProject/NativePlugin/Assets/KARA/Scripts/PluginTest.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PluginTest : MonoBehaviour
+{
+    private static readonly string className = "com.github.karais89.nativeplugin.UnityNativePlugin";
+    private AndroidJavaObject _plugin;
+    string messge = "init";
+    
+    private void Start()
+    {
+        // create Instance        
+        using (AndroidJavaClass anClass = new AndroidJavaClass(className))
+        {
+            _plugin = anClass.CallStatic<AndroidJavaObject>("getInstance");
+            if (_plugin == null)
+            {
+                Debug.Log("plugin instance is null");
+                return;
+            }
+            _plugin.Call("init", this.gameObject);
+        }
+    }
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(100, 100, 100, 50), "testAndroidFunc"))
+        {
+            _plugin.Call("testAndroidFunc", "PluginTest");
+        }
+
+        if (GUI.Button(new Rect(100, 200, 100, 50), "testJavaFuncReturnInt"))
+        {
+            messge += ("\n" + _plugin.Call<int>("testJavaFuncReturnInt"));
+        }
+
+        if (GUI.Button(new Rect(100, 300, 100, 50), "testJavaFuncReturnStr"))
+        {
+            messge += ("\n" + _plugin.Call<string>("testJavaFuncReturnStr", 70000));
+        }
+
+        GUI.Label(new Rect(Screen.width / 2 - 350, Screen.height / 2 - 150, 700, 300), messge);
+    }
+
+    public void SetAndroidLog(string message)
+    {
+        messge += ("\n" + message);
+    }
+}
